59b17c9c65a7768adc4a94708417e884baa176bb
Jude Wells <judewells@hotmail.com>
2022-07-21 19:38:46 +0100

    updating the qsub file

---
+++ no_gpu_t5.qsub.sh
@@ -0,0 +1,33 @@
+#   For further info please read http://hpc.cs.ucl.ac.uk
+#   For cluster help email cluster-support@cs.ucl.ac.uk
+#
+#   NOTE hash dollar is a scheduler directive not a comment.
+
+# These are flags you must include - Two memory and one runtime.
+# Runtime is either seconds or hours:min:sec
+# This version of the script runs without GPU
+# change line beginning -t 1-40: the number 40 should be the number of rows in your dataset
+# change line beginning -t wd: this is your working directory e.g. home
+# change line source t5_env/bin/activate so that it activates the path to your virtual environment
+
+
+#$ -l tmem=16G
+#$ -l h_rt=2:30:02
+#$ -t 1-40
+#These are optional flags but you probably want them in all jobs
+
+#$ -S /bin/bash
+#$ -j y
+#$ -N t5
+#$ -wd /home/jwells/t5_basic
+#The code you want to run now goes here.
+
+hostname
+date
+#export PATH=/share/apps/python-3.8.5-shared/bin:$PATH
+#export LD_LIBRARY_PATH=/share/apps/python-3.8.5-shared/lib:$LD_LIBRARY_PATH
+# source /share/apps/source_files/python/python-3.8.5.source
+source t5_env/bin/activate
+python3 --version
+python3  main.py $SGE_TASK_ID
+date
